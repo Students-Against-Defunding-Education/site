0cc078b0f274f5f0b2f03fda2d5d715e251978ae
atsoll <sollazzo@ualberta.ca>
2020-12-06 19:46:48 -0800

    attempt cors fix

--- js/env.js
+++ js/env.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = 'http://34.68.158.190';
+  window.__env.apiUrl = ' https://cors-anywhere.herokuapp.com/http://34.68.158.190';
   window.__env.apiKey = 'm:o+f8{~#xEmK:B28OnR8^(m_%y4A2';
 
 }(this));
